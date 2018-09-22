90079188658efdf20ed1bd0f640212f03d46c2bd
Sirlean Souza <sirleansantos_@hotmail.com>
2018-09-22 18:38:54 -0300

    Removendo Using desnecessários

--- 1 - PROJETO/SosDentes/Telas/frmAgenda.cs
+++ 1 - PROJETO/SosDentes/Telas/frmAgenda.cs
@@ -1,15 +1,8 @@
-﻿using SosDentes.BD;
-using SosDentes.ClnNegocios;
+﻿using SosDentes.ClnNegocios;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace SosDentes.Telas
 {
